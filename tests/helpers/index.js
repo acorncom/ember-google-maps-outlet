5134c9d9ba9c3a3a5028db08cfd1897c4d096cb3
David Baker <dbaker@acornwebconsultants.com>
2025-04-20 16:26:16 +0300

    Upgrade to Ember 5.12, drop Ember 3.28 testing in favor of 4.12+ support

--- tests/helpers/index.js
+++ tests/helpers/index.js
@@ -1,10 +1,10 @@
 import {
-  setupApplicationTest,
-  setupRenderingTest,
-  setupTest,
+  setupApplicationTest as upstreamSetupApplicationTest,
+  setupRenderingTest as upstreamSetupRenderingTest,
+  setupTest as upstreamSetupTest,
 } from 'ember-qunit';
 
-// This file exists to provide wrappers around ember-qunit's / ember-mocha's
+// This file exists to provide wrappers around ember-qunit's
 // test setup functions. This way, you can easily extend the setup that is
 // needed per test type.
 
@@ -23,7 +23,7 @@ function setupApplicationTest(hooks, options) {
   // This is also a good place to call test setup functions coming
   // from other addons:
   //
-  // setupIntl(hooks); // ember-intl
+  // setupIntl(hooks, 'en-us'); // ember-intl
   // setupMirage(hooks); // ember-cli-mirage
 }
 
